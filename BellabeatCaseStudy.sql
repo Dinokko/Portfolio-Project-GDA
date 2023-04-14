d03782d951d49ecb37ecc0d85a9449a1ace52e16
Dinokko <74656281+Dinokko@users.noreply.github.com>
2023-04-13 23:10:51 -0700

    Update BellabeatCaseStudy.sql

--- BellabeatCaseStudy.sql
+++ BellabeatCaseStudy.sql
@@ -3,5 +3,16 @@
 # Checked for distinct values in DailyActivity
 
 SELECT DISTINCT Id FROM DailyActivity
+# Pulled 33 results
 
-# 
+# Checked for distinct values in WeightLogInfo
+
+SELECT DISTINCT Id FROM WeightLogInfo
+# Pulled 8 results
+
+# Checked for distinct values in SleepLog
+
+SELECT DISTINCT Id FROM SleepLog
+# Pulled 24 results
+
+
